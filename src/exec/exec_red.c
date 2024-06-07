393cac2254894d694b17112b7916e4bb207322c2
Ismael Henrique Da Silva <ismaelhrsilva@gmail.com>
2024-06-07 18:06:28 -0300

    reds -less heredoc- implemented

--- src/exec/exec_red.c
+++ src/exec/exec_red.c
@@ -6,7 +6,7 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 20:44:17 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/06/06 22:26:20 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/06/07 18:04:19 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ static void ft_close_fd_tmp(const int *fd)
 	close(fd[0]);
 	close(fd[1]);
 }
-
 
 static void	ft_change_fds(int fd, int new)
 {
@@ -42,25 +41,27 @@ static void	ft_which_red(t_node *root, int flag)
 		root->fd = open(root->right->str, O_RDONLY, 0644);
 }
 
-
 static void	ft_redirects(t_node *root, t_shell *shell)
 {
-	if (ft_value_int(root->left->phrase, 0, 1) & REDALL)
+	int	flag;
+
+	if (root->left->type & REDALL)
 		ft_redirects(root->left, shell);
+	flag |= root->type;
 	if (root->type & (REDOUT | APPEND) &&
 		(root->left->fd != -1 || g_status == 0))
 	{
-		ft_which_red(root, REDOUT);
-		ft_which_red(root, APPEND);
+		ft_which_red(root, flag);
+		ft_which_red(root, flag);
 		ft_change_fds(root->fd, STDOUT_FILENO);
 	}
-	if (root->type & (REDIN | HEREDOC) &&
+	else if (root->type & (REDIN | HEREDOC) &&
 		(root->left->fd != -1 || g_status == 0))
 	{
-		ft_which_red(root, REDIN);
+		ft_which_red(root, flag);
 		ft_change_fds(root->fd, STDOUT_FILENO);
 	}
-	if (root->left->fd == -1)
+	else if (root->left->fd == -1)
 		root->fd = -1;
 }
 
