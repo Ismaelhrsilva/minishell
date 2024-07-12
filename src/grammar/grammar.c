240dfcc75270ffca4e68cf50f580fce4edf13dd1
Ismael Henrique Da Silva <ismaelhrsilva@gmail.com>
2024-07-12 18:04:26 -0300

    aspas error syntax ok

--- src/grammar/grammar.c
+++ src/grammar/grammar.c
@@ -6,7 +6,7 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 18:38:12 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/07/12 15:27:58 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/07/12 18:00:16 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,44 +35,43 @@ static int	ft_metacharacter_edges(t_vector *phrase, size_t pos)
 	return (0);
 }
 
-char	*ft_which_sinal(char str, char *signal, int *s)
+char	*ft_which_sinal(char str, char *signal)
 {
 	if (ft_strchr("\"", str))
-	{
 		signal = "\"";
-		*s = 1;
-	}
 	else if (ft_strchr("\'", str))
-	{
 		signal = "\'";
-		*s = 1;
-	}
+	else
+		signal = NULL;
 	return (signal);
 }
 
-static int	ft_valide_quotes(t_vector *phrase, size_t pos, int i, int s)
+static int	ft_valide_quotes(char *str, int i, char *signal)
 {
-	char	*signal;
-	char	*str;
+	int	s;
 
-	signal = NULL;
-	str = ft_value(phrase, pos, 0);
+	s = 0;
 	while (str[i] != '\0')
 	{
-		signal = ft_which_sinal(str[i], signal, &s);
-		i++;
-		if (ft_count_chr(signal, '\'') == 1 || ft_count_chr(signal, '\"') == 1)
+		signal = ft_which_sinal(str[i++], signal);
+		if (str[i] != '\0' && (ft_count_chr(signal, '\'') == 1
+				|| ft_count_chr(signal, '\"') == 1))
 		{
-			while (!ft_strchr(signal, str[i]) && str[i] != '\0')
+			if (str[i] != '\0' && (str[i] == '\'' || str[i] == '\"'))
 				i++;
-			if (str[i] == '\'' || str[i] == '\"')
-				s = 0;
-			if (s == 1)
-				return (1);
 			else
-				return (0);
+			{
+				while (!ft_strchr(signal, str[i]) && str[i] != '\0')
+					i++;
+				if (!(str[i] == '\'' || str[i] == '\"'))
+					s = 1;
+				if (str[i] != '\0')
+					i++;
+			}
 		}
 	}
+	if (s == 1)
+		return (1);
 	return (0);
 }
 
@@ -85,7 +84,7 @@ int	ft_grammar_rules(t_vector *phrase)
 	{
 		if (error(ft_metacharacter_edges(phrase, i))
 			|| error(ft_metacharacter_following(phrase, i))
-			|| error(ft_valide_quotes(phrase, i, 0, 0)))
+			|| error(ft_valide_quotes(ft_value(phrase, i, 0), 0, NULL)))
 			return (0);
 		i++;
 	}
